bf355f866ab260c746b30de07ef1a451efd0a6ef
willychristofery <sihotangwilly@gmail.com>
2020-07-05 20:30:11 +0700

    done public/erd.png

---
+++ database/migrations/2020_07_05_125226_create_artikel_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtikelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artikel', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('judul');
+            $table->string('isi');
+            $table->string('slug');
+            $table->string('tag');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artikel');
+    }
+}
